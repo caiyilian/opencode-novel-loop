c498d4d08eb1cadcc9331d5dad686dae80e3b88b
蔡依炼 <87569603+caiyilian@users.noreply.github.com>
2026-05-19 21:19:13 +0800

    update

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@ python -m dialoop.cli novel.txt \
 --output labeled_test.txt \
 --batch-size 1 \
 --max-tool-steps 20 \
---max-iterations 46 \
+--max-iterations 31 \
 --protocol auto \
 --base-url http://172.31.102.237:11434/v1 \
 --api-key ollama \
---model qwen3:32b
+--model qwen3:32b \
+--model-timeout 60
