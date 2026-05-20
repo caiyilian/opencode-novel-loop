0c0e1cf7a2ec027e524a29500516988da02940f8
蔡依炼 <87569603+caiyilian@users.noreply.github.com>
2026-05-21 00:33:42 +0800

    update

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ python -m dialoop.cli novel.txt \
 --output labeled_test.txt \
 --batch-size 1 \
 --max-tool-steps 20 \
---max-iterations 2 \
+--max-iterations 291 \
 --protocol auto \
 --base-url http://172.31.102.237:11434/v1 \
 --api-key ollama \
